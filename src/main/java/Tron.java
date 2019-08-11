b8ebcabf2404c3a13bcd0bcbcd6c8c9a77b5518b
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2019-08-11 14:16:31 -0500

    first commit

---
+++ src/main/java/Tron.java
@@ -0,0 +1,15 @@
+/*
+File: Tron.java
+Created: 11/08/2019
+Author: Hisbaan Noorani
+*/
+
+public class Tron {
+    public static void main(String[] args) {
+        new Tron();
+    }
+
+    Tron() {
+
+    }
+}
